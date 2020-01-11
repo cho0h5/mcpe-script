6b5ef7ff9f5787be84144fb8d68ae62e26ba8d65
Youngho Cho <dominoyh5@gmail.com>
2020-01-11 16:40:10 +0900

    Complete more-mod

--- more-mob.js
+++ more-mob.js
@@ -16,7 +16,23 @@ function entityRemovedHook(entity) {
 
 function modTick() {
   const time = Level.getTime();
-  if((12000 < time) && (time < 24000)) {
+  //if((12000 < time) && (time < 24000)) {
+  if(true) {//Test
+    if(coolTime < 0) {
+      for(var player in players) {
+        var x = Entity.getX(player);
+        var y = Entity.getY(player);
+        var z = Entity.getZ(player);
+        for(var i = 0; i < 5; i++) {
+          x += Math.round(Math.random()*10);
+          y += Math.round(Math.random()*10);
+          z += Math.round(Math.random()*10);
+          Entity.spawnMob(x, y, z, typeId, skin);
+        }
+      }
+      //coolTime = 20*5*60;
+      coolTime = 20*5;//Test
+    }
     coolTime -= 1;
   }
 }
